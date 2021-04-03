b05f9cd93702f86f6e10279c495bd43afed57fdf
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-04-03 20:06:28 +1100

    Fix errors with html sheets and static files

--- breakconnect/views.py
+++ breakconnect/views.py
@@ -31,23 +31,3 @@ users = [
 def sign_in(request):
     context = {}
     return render(request, 'breakconnect/sign-in.html', context)
-
-def sign_up(request):
-    context = {}
-    return render(request, 'breakconnect/sign-up.html', context)
-    
-def reset_password(request):
-    context = {}
-    return render(request, 'breakconnect/reset-password.html', context)
-
-def start_break(request):
-    context = {}
-    return render(request, 'breakconnect/start-break.html', context)
-
-def start_chat(request):
-    context = {}
-    return render(request, 'breakconnect/start-chat.html', context)
-
-def current_chat(request):
-    context = {}
-    return render(request, 'breakconnect/current-chat.html', context)
