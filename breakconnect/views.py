f7648e7855b9a1a09d931df5c4e73d8ccc4cd826
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-04-03 19:34:44 +1100

    Working on setting up static files

--- breakconnect/views.py
+++ breakconnect/views.py
@@ -28,11 +28,26 @@ users = [
     }
 ]
 
-def home(request):
-    context = {
-        'users': users
-    }
-    return render(request, 'breakconnect/home.html', context)
+def sign_in(request):
+    context = {}
+    return render(request, 'breakconnect/sign-in.html', context)
 
-def about(request):
-    return HttpResponse("<h1> Break Connnect About </h1>")
+def sign_up(request):
+    context = {}
+    return render(request, 'breakconnect/sign-up.html', context)
+    
+def reset_password(request):
+    context = {}
+    return render(request, 'breakconnect/reset-password.html', context)
+
+def start_break(request):
+    context = {}
+    return render(request, 'breakconnect/start-break.html', context)
+
+def start_chat(request):
+    context = {}
+    return render(request, 'breakconnect/start-chat.html', context)
+
+def current_chat(request):
+    context = {}
+    return render(request, 'breakconnect/current-chat.html', context)
