12c27321d5a16017ac0d031efae82c1fc43283d2
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-04-03 20:31:28 +1100

    add remaining html templates, not all are formatted correctly

--- breakconnect/urls.py
+++ breakconnect/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.sign_in, name = "breakconnect-sign-in"),
+    path('signup/', views.sign_up, name = "breakconnect-sign-up"),
+    path('reset/', views.reset_password, name = "breakconnect-reset-password"),
+    path('startbreak/', views.start_break, name = "breakconnect-about"),
+    path('startchat/', views.start_chat, name = "breakconnect-about"),
+    path('currentchat/', views.current_chat, name = "breakconnect-about"),
 ]
