02cd33e8acdc0cd4aa1367a08136a82ac33ba1c8
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-04-04 02:18:00 +1100

    Add links between pages

--- breakconnect/views.py
+++ breakconnect/views.py
@@ -1,33 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-users = [
-    {
-        'email': 'person1@email.com',
-        'password': 'password',
-        'nameFamily': 'Smith',
-        'nameGiven': 'Mary',
-        'gender': 'Female',
-        'yearBorn': '1990'
-    },
-    {
-        'email': 'person2@email.com',
-        'password': 'password',
-        'nameFamily': 'Smith',
-        'nameGiven': 'Mark',
-        'gender': 'Male',
-        'yearBorn': '1992'
-    },
-    {
-        'email': 'person3@email.com',
-        'password': 'password',
-        'nameFamily': 'Smith',
-        'nameGiven': 'Tom',
-        'gender': 'Male',
-        'yearBorn': '1995'
-    }
-]
-
 def sign_in(request):
     context = {}
     return render(request, 'breakconnect/sign-in.html', context)
