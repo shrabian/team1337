6d801b76ad048c3b809b9d366335df03140885da
Sharan Krishnan <sharankrishnan6@gmail.com>
2021-04-05 01:47:28 +1000

    profile model

--- usersapp/apps.py
+++ usersapp/apps.py
@@ -3,3 +3,6 @@ from django.apps import AppConfig
 
 class UsersappConfig(AppConfig):
     name = 'usersapp'
+
+    def ready(self):
+        import usersapp.signals
--- usersapp/models.py
+++ usersapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+num_messages_displayed = 10
+
+user = get_user_model()
+
+class Text(models.Model):
+    timestamp = models.DateTimeField(auto_now_add = True)
+    author = models.ForeignKey(user, related_name = 'author', on_delete =  models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return self.author.username
+
+    def last_x_messages(self):
+        return Text.objects.order_by('-timestamp').all()[:num_messages_displayed]
