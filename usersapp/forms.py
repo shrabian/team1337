ce3b167cecb600032691b5f3517afb926b5773c0
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-04-04 19:32:25 +1000

    Fixed user sign up

--- usersapp/forms.py
+++ usersapp/forms.py
@@ -3,20 +3,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
-    orgcode = forms.CharField(label="orgcode")
-    fullname = forms.CharField(label="fullname")
+    email = forms.EmailField(label="email")
 
     class Meta:
         model = User
-        fields = ['fullname', 'email', 'orgcode', 'password1', 'password2']
-
-    def save(self, request):
-        user = super(UserRegisterForm, self).save(request)
-        first_name, last_name = self.cleaned_data["fullname"].split()
-        user.first_name = first_name
-        user.last_name = last_name
-        user.email = self.cleaned_data["email"]
-        user.organisation_code = self.cleaned_data["orgcode"]
-        user.save()
-        return user
+        fields = ['username', 'email', 'password1', 'password2']
--- usersapp/views.py
+++ usersapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from .forms import UserRegisterForm
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 def sign_in(request):
@@ -9,11 +11,11 @@ def sign_in(request):
     return render(request, 'usersapp/sign-in.html', context)
 
 def sign_up(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('blog-home')
+            return redirect('usersapp:sign-in')
     else:
         form = UserRegisterForm()
     return render(request, 'usersapp/sign-up.html', {'form': form})
