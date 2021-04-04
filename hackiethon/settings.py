cbbd89cb03d50bb1ba9f194cf0338ccbf1d3c004
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-04-05 06:16:08 +1000

    staticfiles

--- hackiethon/settings.py
+++ hackiethon/settings.py
@@ -125,10 +125,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 ]
+
+os.makedirs(STATIC_TMP, exist_ok=True)
+os.makedirs(STATIC_ROOT, exist_ok=True)
+
 
 LOGIN_REDIRECT_URL='/'
 
