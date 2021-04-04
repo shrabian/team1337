db1d2e650c6a270e5a52fb918f2a794c5ed709d8
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-04-04 14:42:32 +1000

    Add users app to with django server

--- hackiethon/settings.py
+++ hackiethon/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'breakconnect.apps.BreakconnectConfig',
+    'crispy_forms',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
