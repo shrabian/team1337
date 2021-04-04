b48d21d76d4e6d741d32c60604e0d9496cef48a5
DanielDigby <50546545+DanielDigby@users.noreply.github.com>
2021-04-05 02:15:05 +1000

    new models

--- breakconnect/models.py
+++ breakconnect/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Break(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    break_start = models.TimeField().auto_now_add
+    bio = models.CharField(max_length = 200, default = "", primary_key = True)
--- usersapp/models.py
+++ usersapp/models.py
@@ -4,10 +4,6 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
     image = models.ImageField(default = 'default.jpg', upload_to="profile_pics")
-    bio = models.CharField(max_length = 200, default = "", primary_key = True)
-
-    def __str__(self):
-        return f"{self.user.username} Profile"
 
 
 # Create your models here.
